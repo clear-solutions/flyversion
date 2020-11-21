fd16fef03e74a458f9b05f8805e1a900f554fcff
maxdanylenko <danylenko.max@gmail.com>
2020-11-21 17:33:38 +0200

    updated java version to 15

--- src/test/java/com/clearsolutions/flyversion/CalculatorTest.java
+++ src/test/java/com/clearsolutions/flyversion/CalculatorTest.java
@@ -14,6 +14,7 @@ class CalculatorTest {
     void addsTwoNumbers() {
         Calculator calculator = new Calculator();
         assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+        System.out.println("sfds");
     }
 
     @ParameterizedTest(name = "{0} + {1} = {2}")
